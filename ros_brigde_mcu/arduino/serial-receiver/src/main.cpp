ca25622526a07515deeec940a0abc1346c9e4f60
HUST-Qkhanh <kdamquoc@gmail.com>
2023-06-11 14:20:13 +0700

    modified:   ros_brigde_mcu/arduino/serial-receiver/src/main.cpp 	modified:   ros_brigde_mcu/ros_brigde_mcu/__pycache__/ros_send_serial.cpython-310.pyc 	modified:   ros_brigde_mcu/ros_brigde_mcu/ros_send_serial.py

--- ros_brigde_mcu/arduino/serial-receiver/src/main.cpp
+++ ros_brigde_mcu/arduino/serial-receiver/src/main.cpp
@@ -5,16 +5,39 @@ union {
   byte b[4];
 } data;
 
+float linear_x;
+float linear_y;
+float angular_z;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available() >= 4) {
-    for (int i = 0; i < 4; i++) {
-      data.b[i] = Serial.read();
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == 0x36) {
+      for (int i = 0; i < 4; i++) {
+        data.b[i] = Serial.read();
+      }
+      linear_x = data.f;
+      
+      for (int i = 0; i < 4; i++) {
+        data.b[i] = Serial.read();
+      }
+      linear_y = data.f;
+      
+      for (int i = 0; i < 4; i++) {
+        data.b[i] = Serial.read();
+      }
+      angular_z = data.f;
+      
+      Serial.print("linear_x: ");
+      Serial.println(linear_x);
+      Serial.print("linear_y: ");
+      Serial.println(linear_y);
+      Serial.print("angular_z: ");
+      Serial.println(angular_z);
     }
-    Serial.print("Received: ");
-    Serial.println(data.f);
   }
 }
--- ros_brigde_mcu/ros_brigde_mcu/ros_send_serial.py
+++ ros_brigde_mcu/ros_brigde_mcu/ros_send_serial.py
@@ -21,22 +21,20 @@ class ros_send_serial(Node):
         self.subscription  # prevent unused variable warning
 
     def ros_send_serial_callback(self, msg):
-        linear_x=struct.pack('f',msg.linear.x) #convert to bytes
-        linear_y=msg.linear.y
-        angular_z=msg.angular.z
+        linear_x=struct.pack('cf','$'.encode(), msg.linear.x) #convert to bytes
+        #linear_y=struct.pack('f',msg.linear.y)
+        #angular_z=struct.pack('f',msg.angular.z)
 
-        send_decode=struct.unpack('f',linear_x)
+        #send_decode=struct.unpack('f',linear_x)
         self.get_logger().info('I send: "%s"' % linear_x)
 
-        serialPort.write(b'$')
+        #serialPort.write(b'$')
         serialPort.write(linear_x)
+        #serialPort.write(linear_y)
+        #serialPort.write(angular_z)
         #serialPort.write(b"%f",linear_y)
         #serialPort.write(b"%f",angular_z)
 
-        ser=serialPort.read(4)
-        ser_decode=ser.decode('utf-8')
-        self.get_logger().info('I heard: "%s"' % ser)
-        self.get_logger().info('decode to: "%s"' % ser_decode)
 
         time.sleep(1)
         serialPort.flush()
