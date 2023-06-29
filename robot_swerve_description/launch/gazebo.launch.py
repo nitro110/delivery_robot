801272a874ce6b0507d06fa12cfd5df913813275
LAV2000 <luonganhvan2000@gmail.com>
2023-06-29 23:12:20 +0700

    lav

---
+++ robot_swerve_description/launch/gazebo.launch.py
@@ -0,0 +1,37 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+    pkg_name = 'robot_swerve_description'
+    file_subpath = 'description/robot.urdf.xacro'
+
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw, 'use_sim_time': True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py'])
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+                        output='screen')
+
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity
+    ])
