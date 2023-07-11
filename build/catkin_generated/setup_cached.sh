04237bf4386693f2064c8337d0835728040eba97
HUST-Qkhanh <kdamquoc@gmail.com>
2023-07-11 21:33:16 +0700

    renamed:    build/.cmake/api/v1/reply/cmakeFiles-v1-e98123cebb5eb6185bca.json -> build/.cmake/api/v1/reply/cmakeFiles-v1-8adceb9cf539f1adef4e.json

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,6 +9,9 @@ export ROSLISP_PACKAGE_DIRECTORIES='/home/khanh/khanh_repo/src/delivery_robot/bu
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/khanh/khanh_repo/src/delivery_robot/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/khanh/khanh_repo/src/delivery_robot/build/devel/lib:$LD_LIBRARY_PATH"
 export PATH='/opt/ros/noetic/bin:/home/khanh/.local/bin:/home/khanh/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/khanh/khanh_repo/src/delivery_robot/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/khanh/khanh_repo/src/delivery_robot/build'
+export PYTHONPATH="/home/khanh/khanh_repo/src/delivery_robot/build/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROS_PACKAGE_PATH="/home/khanh/khanh_repo/src:$ROS_PACKAGE_PATH"
