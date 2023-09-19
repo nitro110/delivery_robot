76ad0f10eb4f2de74969c2785029645b60d37e82
HUST-Qkhanh <kdamquoc@gmail.com>
2023-09-19 14:44:30 +0700

    addcomment

--- swerve_controller/launch/swerve_control_odom.launch.py
+++ swerve_controller/launch/swerve_control_odom.launch.py
@@ -6,21 +6,23 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 
 
+# launch LaunchDescription define
 def generate_launch_description():
     ld = LaunchDescription()
     config = os.path.join(
         get_package_share_directory("swerve_controller"),
         "config",
-        "robot_geometry.yaml",
+        "robot_geometry.yaml",  # get parameters from yaml file
     )
 
+    # load robot swerve controller
     node_swerve_commander = Node(
         package="swerve_controller",
         name="swerve_commander",
         executable="wheel_cmd_publisher",
         parameters=[config],
     )
-
+    # load robot odom calculator
     node_swerve_odom = Node(
         package="swerve_controller",
         name="swerve_odometer",
